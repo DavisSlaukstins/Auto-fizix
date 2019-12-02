cf77526a57b8487e5fadbfffa99657624caec57c
DavisSlaukstins <48949243+DavisSlaukstins@users.noreply.github.com>
2019-12-02 21:41:24 +0200

    Update gg.py

--- gg.py
+++ gg.py
@@ -5,7 +5,16 @@ import smbus2
 import bme280
 import math
 
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+
+calibration_params = bme280.load_calibration_params(bus, address)
+
+data = bme280.sample(bus, address, calibration_params)
+
 buzzer_pin = 27
+
 #NOTIS
 notes = {
 	'B0' : 31,
@@ -174,9 +183,101 @@ def play(melody,tempo,pause,pace=0.800):
 if __name__ == '__main__':		# Program start from here
 	try:
 		setup()
-		
+
+		while True:
+        	try:
+        		w = str (input ("Gādā gāzē atradas mērinstruments : "))
+        		break
+    		except ValueError:
+				print("Tāda vērtība nestrādā")
+				GPIO.output(27,GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.LOW)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.LOW)
+		GPIO.output(27,GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(27,GPIO.LOW)
+#pieprasa tilpumu
+
+		while True:
+			try:
+				v = float (input ("Ievadi trauka/telpas tilpumu m3: "))
+				break
+			except ValueError:
+				print("Tāda vērtība nestrādā")
+				GPIO.output(27,GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.LOW)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.LOW)
+		GPIO.output(27,GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(27,GPIO.LOW)
+
+#pieprasa vielas molmasu
+
+		while True:
+			try:
+				M1 = float (input ("Ievadi gāzes molmasu g/mol: "))
+				break
+			except ValueError:
+				print("Tāda vērtība nestrādā")
+				GPIO.output(27,GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.LOW)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(27,GPIO.LOW)
+		GPIO.output(27,GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(27,GPIO.LOW)
+
+# w = vielas nosaukums, v = tilpums, m = molmasa.
+#tilpums *spiediens = masa/ mol masa * konstante *temperatura
+#masa = spiediens*tilpums*molmasa/r*t
+#v2 = 3RT/M
+
+#definēju lietas
+		M = M1/1000
+		r = 8.31
+		T = data.temperature
+		p1 = data.pressure
+		t = T+273
+#veicu apreikinus
+		p = p1*100
+		m1 = v*p*M
+		m2 = r*t
+		m = m1/m2
+		v2 = 3*r*t/M
+		v = math.sqrt(v2)
+
+#paradu nepieciesamo
+		print("")
+		print("Veicu apreikinus")
+		print("")
 		play(melody, tempo, 0.3, 0.800)
-		time.sleep(2)
+			time.sleep(2)
+		print("")
+		print("Pabeidzu!")
+		print("")
+		print("")
+		print(w," masa ir ", m,"kg")
+		print("")
+		print(w," ātrums ir ", v,"m/s")
+		print("")
+#print(data.timestamp)
+#print(data.temperature)
+#print(data.pressure)
+#print(data.humidity)
+
+		print(data)
+		
 		
 		destroy()
 	except KeyboardInterrupt:  	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
